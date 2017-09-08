c5c9f472bdc534c78905482f7c84487768704b93
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-09-08 15:16:31 -0700

    Add login screen to get instagram token

---
+++ app/containers/Gamepad.js
@@ -0,0 +1,61 @@
+'use strict'
+
+import React, {Component} from 'react'
+import {
+  Dimensions,
+  Text,
+  Image,
+  View,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native'
+
+const {width, height} = Dimensions.get('window')
+
+export default class Gamepad extends Component {
+  render() { return (
+    <View style={style.container}>
+      <View style={style.tray}>
+        <TouchableOpacity onPress={() => alert('no')}>
+          <Text style={style.button}>No</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => alert('yes')}>
+          <Text style={style.button}>Yes</Text>
+        </TouchableOpacity>
+      </View>
+      <ScrollView style={[style.container]}>
+        <Image style={style.image} resizeMode="cover" source={{url: 'https://scontent-lax3-1.cdninstagram.com/t51.2885-15/e35/21372067_129553654347748_5155457396683833344_n.jpg'}} />
+        <Image style={style.image} resizeMode="cover" source={{url: 'https://scontent-lax3-1.cdninstagram.com/t51.2885-15/e35/21147329_1973484256261473_9031074582802464768_n.jpg'}} />
+        <Image style={style.image} resizeMode="cover" source={{url: 'https://scontent-lax3-1.cdninstagram.com/t51.2885-15/e35/21433986_1471057176276621_7259829455751741440_n.jpg'}} />
+      </ScrollView>
+    </View>
+  )}
+}
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  tray: {
+    backgroundColor: 'hotpink',
+    position: 'absolute',
+    top: height - 50,
+    height: 50,
+    zIndex: 1,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+
+  button: {
+    color: 'white',
+  },
+
+  image: {
+    height: height,
+    width: width,
+  }
+})
---
+++ app/containers/Login.js
@@ -0,0 +1,75 @@
+'use strict'
+
+import React, {Component} from 'react'
+import {
+  Linking,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native'
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props)
+    this.login  = this.login.bind(this)
+    this.handle = this.handle.bind(this)
+  }
+
+  componentDidMount() {
+    Linking.addEventListener('url', this.handle)
+  }
+
+  handle() {
+    Linking.removeEventListener('url', this.handle)
+  }
+
+  handle(event) {
+    if( !event.url || !event.url.match(/mayte:\/\/ig/) ) {
+      return console.warn('Unknown event url', event && event.url)
+    }
+
+    const matches = event.url.match(/access_token=(.*)/)
+    if( !matches ) {
+      return console.warn('No access token provided', event && event.url)
+    }
+
+    console.warn('Access Token', matches[1])
+  }
+
+  login() {
+    const redirectUrl = 'http://localhost:3000/webhooks/instagram'
+    const clientId    = '1c6d8f10063b4ac7b9010194c380b6fb'
+
+    const url = 'https://instagram.com/oauth/authorize/?client_id='+clientId+
+        '&redirect_uri='+redirectUrl+
+        '&response_type=token'
+    Linking.canOpenURL(url).then(supported => {
+      if (!supported) {
+        console.log('Can\'t handle url: ' + url)
+      } else {
+        return Linking.openURL(url)
+      }
+    }).catch(console.error)
+  }
+
+  render() { return (
+    <View style={style.container}>
+      <TouchableOpacity onPress={this.login}>
+        <Text style={style.button}>Login with Instagram</Text>
+      </TouchableOpacity>
+    </View>
+  )}
+}
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  button: {
+    color: 'blue',
+  },
+})
