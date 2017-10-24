3cc254846d68a762b5bcd3a107c0db229e3ccaa7
benBen <benmbenjamin@gmail.com>
2017-10-24 12:25:03 -0700

    move style to inside component so it can be sensitive to props and also have nested styles

--- app/components/RecsView.js
+++ app/components/RecsView.js
@@ -19,6 +19,97 @@ const {width, height} = Dimensions.get('window')
 
 export default function(props) {
   const imgStyle = {width: width, height: props.viewHeight}
+  const style = {
+    container: {
+      flex: 1,
+    },
+
+    info: {
+      gradient: {
+        position: 'absolute',
+        width: '100%',
+        height: props.infoOpen ? height : 'auto',
+        top: props.infoOpen ? 0 : height - 235,
+      },
+      cont: {
+        flex: 1,
+        paddingLeft: 20,
+        paddingRight: 20,
+      },
+    },
+
+    handle: {
+      textAlign: 'center',
+      backgroundColor: 'transparent',
+      color: 'rgba(255,255,255,1)',
+      fontSize: 23,
+    },
+
+    bio: {
+      textAlign: 'center',
+      backgroundColor: 'transparent',
+      color: 'rgba(255,255,255,1)',
+      fontSize: 18,
+      paddingTop: 40,
+    },
+
+    tray: {
+      width: '100%',
+      paddingTop: 40,
+      paddingBottom: 40,
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      alignItems: 'center',
+    },
+
+    bubble: {
+      width: width * 0.3,
+      height: width * 0.125,
+      borderRadius: width * 0.125,
+      borderWidth: 1,
+      borderColor: 'rgba(255,255,255,0.9)',
+      backgroundColor: 'rgba(255,255,255,0.2)',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+
+    icon: {
+      height: '60%',
+      opacity: 0.8,
+    },
+
+    button: {
+      color: 'rgba(255,255,255,0.9)',
+      backgroundColor: 'rgba(255,255,255,0)',
+    },
+
+    centered: {
+      flex:           1,
+      alignItems:     'center',
+      justifyContent: 'center',
+    },
+
+    error: {
+      color: 'red',
+    },
+
+    name: {
+      top: 0, left: 0, right: 0,
+      color: 'rgba(255,255,255,1)',
+      backgroundColor: 'transparent',
+      textAlign: 'center',
+      fontSize: 25,
+      zIndex: 1,
+      paddingTop: 30,
+    },
+
+    location: {
+      textAlign: 'center',
+      fontSize: 20,
+      backgroundColor: 'transparent',
+      color: 'rgba(255,255,255,1)',
+    }
+  }
 
   return (
     <View style={style.container}>
@@ -52,98 +143,41 @@ export default function(props) {
                      source={{url: p.url}} />
             ))}
           </ScrollView>
-          <LinearGradient colors={['rgba(0,0,0,0)', 'rgba(0,0,0,0.6)']} style={style.info}>
-            <Text style={style.name}>
-              {props.recs[props.index].fullName.split(' ')[0]}, {20 + Math.floor(Math.random() * 10)}
-            </Text>
-            <Text style={style.location}>
-              {Math.ceil(Math.random() * 5)} miles away
-            </Text>
-            <View style={[style.tray]}>
-              <TouchableOpacity style={[style.bubble]} onPress={() => props.pass(props.recs[props.index].id)}>
-                <Image style={style.icon}
-                       resizeMode='contain'
-                       source={require('../images/x.png')} />
-              </TouchableOpacity>
-              <TouchableOpacity style={[style.bubble]} onPress={() => props.like(props.recs[props.index].id)}>
-                <Image style={style.icon}
-                       resizeMode='contain'
-                       source={require('../images/heart.png')} />
-              </TouchableOpacity>
-            </View>
+          <LinearGradient colors={['rgba(0,0,0,0)', 'rgba(0,0,0,0.6)']} style={style.info.gradient}>
+            <ScrollView style={style.info.cont}>
+              <Text style={style.name}>
+                {props.recs[props.index].fullName.split(' ')[0]}, {20 + Math.floor(Math.random() * 10)}
+              </Text>
+              <Text style={style.location}>
+                {Math.ceil(Math.random() * 5)} miles away
+              </Text>
+              <View style={[style.tray]}>
+                <TouchableOpacity style={[style.bubble]} onPress={() => props.pass(props.recs[props.index].id)}>
+                  <Image style={style.icon}
+                         resizeMode='contain'
+                         source={require('../images/x.png')} />
+                </TouchableOpacity>
+                <TouchableOpacity style={[style.bubble]} onPress={() => props.like(props.recs[props.index].id)}>
+                  <Image style={style.icon}
+                         resizeMode='contain'
+                         source={require('../images/heart.png')} />
+                </TouchableOpacity>
+              </View>
+              <View>
+                <Text style={style.handle}>Profreshional Model, Treats!</Text>
+                <Text style={style.handle}>@beners</Text>
+              </View>
+              <View>
+                <Text style={style.bio}>
+{`She a bad bad n she allllready know
+(ya she know it)
+Whole bank accooount I'll blow it 💸`}
+                </Text>
+              </View>
+            </ScrollView>
           </LinearGradient>
         </View>
       }
     </View>
   )
 }
-
-const style = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-
-  info: {
-    position: 'absolute',
-    width: '100%',
-    bottom: 0,
-  },
-
-  tray: {
-    width: '100%',
-    paddingTop: 20,
-    paddingBottom: 20,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-  },
-
-  bubble: {
-    width: width * 0.3,
-    height: width * 0.125,
-    borderRadius: width * 0.125,
-    borderWidth: 1,
-    borderColor: 'rgba(255,255,255,0.9)',
-    backgroundColor: 'rgba(255,255,255,0.2)',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  icon: {
-    height: '60%',
-    opacity: 0.8,
-  },
-
-  button: {
-    color: 'rgba(255,255,255,0.9)',
-    backgroundColor: 'rgba(255,255,255,0)',
-  },
-
-  centered: {
-    flex:           1,
-    alignItems:     'center',
-    justifyContent: 'center',
-  },
-
-  error: {
-    color: 'red',
-  },
-
-  name: {
-    top: 0,
-    left: 0,
-    right: 0,
-    color: 'rgba(255,255,255,1)',
-    backgroundColor: 'rgba(255,255,255,0)',
-    textAlign: 'center',
-    fontSize: 25,
-    zIndex: 1,
-  },
-
-  location: {
-    textAlign: 'center',
-    fontSize: 20,
-    backgroundColor: 'transparent',
-    color: 'rgba(255,255,255,1)',
-  }
-})
--- app/containers/Recs.js
+++ app/containers/Recs.js
@@ -14,6 +14,7 @@ class Recs extends Component {
       loading: true,
       index: 0,
       viewHeight: 0,
+      infoOpen: true,
     }
   }
 
