00f88c44463cfcc5f88995ecc7e85520821c9933
benBen <benmbenjamin@gmail.com>
2017-10-24 15:37:42 -0700

    animate info open/closed; TODO: disable overflow scrolling on recs

--- app/components/RecInfoView.js
+++ app/components/RecInfoView.js
@@ -1,45 +1,103 @@
 import React, {Component} from 'react'
 import LinearGradient from 'react-native-linear-gradient'
 import {
-  ActivityIndicator,
+  Animated,
   Dimensions,
   Image,
   ScrollView,
-  StyleSheet,
   Text,
   TouchableOpacity,
   TouchableWithoutFeedback,
-  FlatList,
   View,
 } from 'react-native'
 
 const {width, height} = Dimensions.get('window')
 
 class RecInfoView extends Component {
-  render() {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      topValue: new Animated.Value(height * 0.65),
+      heightValue: new Animated.Value(height * 0.3),
+    }
+
+    this.animateOpen = this.animateOpen.bind(this)
+    this.animateClosed = this.animateClosed.bind(this)
+  }
+
+  componentWillUpdate(nextProps, nextState) {
     const {props} = this
+    if (nextProps.infoOpen && !props.infoOpen) {
+      this.animateOpen()
+    } else if (!nextProps.infoOpen && props.infoOpen) {
+      this.animateClosed()
+    }
+  }
+
+  animateOpen() {
+    Animated.timing(
+      this.state.topValue,
+      {
+        toValue: 0,
+        duration: 333,
+      }
+    ).start()
+    Animated.timing(
+      this.state.heightValue,
+      {
+        toValue: height,
+        duration: 333,
+      }
+    ).start()
+  }
+
+  animateClosed() {
+    Animated.timing(
+      this.state.topValue,
+      {
+        toValue: height * 0.65,
+        duration: 333,
+      }
+    ).start()
+    Animated.timing(
+      this.state.heightValue,
+      {
+        toValue: height * 0.3,
+        duration: 333,
+      }
+    ).start()
+  }
+
+  render() {
+    const {props, state} = this
     const style = {
       info: {
-        gradient: {
-          position: 'absolute',
-          width: '100%',
-          height: props.infoOpen ? height : 'auto',
-          top: props.infoOpen ? 0 : height - height * 0.35,
-        },
-        cont: {
-          flex: 1,
-          paddingLeft: 20,
-          paddingRight: 20,
-        },
-        opener: {
-          position: 'absolute',
-          bottom: 0, left: -20,
-          width: width + 20,
-          height: height * 0.5,
-          backgroundColor: 'transparent',
-          zIndex: 1,
-          display: props.infoOpen ? 'none' : 'flex',
-        }
+        position: 'absolute',
+        top: state.topValue,
+        height: state.heightValue,
+      },
+
+      gradient: {
+        position: 'relative',
+        width: '100%',
+        height: '100%',
+      },
+
+      content: {
+        flex: 1,
+        paddingLeft: 20,
+        paddingRight: 20,
+      },
+
+      opener: {
+        position: 'absolute',
+        bottom: 0, left: -20,
+        width: width + 20,
+        height: height * 0.5,
+        backgroundColor: 'transparent',
+        zIndex: 1,
+        display: props.infoOpen ? 'none' : 'flex',
       },
 
       handle: {
@@ -94,7 +152,6 @@ class RecInfoView extends Component {
         backgroundColor: 'transparent',
         textAlign: 'center',
         fontSize: 25,
-        // zIndex: 1,
         paddingTop: 30,
       },
 
@@ -105,50 +162,53 @@ class RecInfoView extends Component {
         color: 'rgba(255,255,255,1)',
       }
     }
+
     return (
-      <LinearGradient colors={['rgba(0,0,0,0)', 'rgba(0,0,0,0.6)']}
-                      style={style.info.gradient}>
-        <ScrollView style={style.info.cont}
-                    scrollEventThrottle={100}
-                    onScroll={(e) => {
-                      const {y} = e.nativeEvent.contentOffset
-                      if (y < 0) {
-                        props.hideInfo()
-                      }
-                    }}
-                    scrollEnabled={props.infoOpen ? true : false}>
-          <Text style={style.name}>
-            {props.recs[props.index].fullName.split(' ')[0]}, {props.recs[props.index].age}
-          </Text>
-          <Text style={style.location}>
-            {props.recs[props.index].distance} miles away
-          </Text>
-          <View style={[style.tray]}>
-            <TouchableOpacity style={style.info.opener} onPress={() => props.showInfo()} />
-            <TouchableOpacity style={[style.bubble]} onPress={() => props.pass(props.recs[props.index].id)}>
-              <Image style={style.icon}
-                     resizeMode='contain'
-                     source={require('../images/x.png')} />
-            </TouchableOpacity>
-            <TouchableOpacity style={[style.bubble]} onPress={() => props.like(props.recs[props.index].id)}>
-              <Image style={style.icon}
-                     resizeMode='contain'
-                     source={require('../images/heart.png')} />
-            </TouchableOpacity>
-          </View>
-          <View>
-            <Text style={style.handle}>Profreshional Model, Treats!</Text>
-            <Text style={style.handle}>@beners</Text>
-          </View>
-          <View>
-            <Text style={style.bio}>
-{`She a bad bad n she allllready know
-(ya she know it)
-Whole bank accooount I'll blow it 💸`}
+      <Animated.View style={style.info}>
+        <LinearGradient colors={['rgba(0,0,0,0)', 'rgba(0,0,0,0.6)']}
+                        style={style.gradient}>
+          <ScrollView style={style.content}
+                      scrollEventThrottle={100}
+                      onScroll={(e) => {
+                        const {y} = e.nativeEvent.contentOffset
+                        if (y < 0) {
+                          props.hideInfo()
+                        }
+                      }}
+                      scrollEnabled={props.infoOpen ? true : false}>
+            <Text style={style.name}>
+              {props.recs[props.index].fullName.split(' ')[0]}, {props.recs[props.index].age}
             </Text>
-          </View>
-        </ScrollView>
-      </LinearGradient>
+            <Text style={style.location}>
+              {props.recs[props.index].distance} miles away
+            </Text>
+            <View style={[style.tray]}>
+              <TouchableOpacity style={style.opener} onPress={() => props.showInfo()} />
+              <TouchableOpacity style={[style.bubble]} onPress={() => props.pass(props.recs[props.index].id)}>
+                <Image style={style.icon}
+                       resizeMode='contain'
+                       source={require('../images/x.png')} />
+              </TouchableOpacity>
+              <TouchableOpacity style={[style.bubble]} onPress={() => props.like(props.recs[props.index].id)}>
+                <Image style={style.icon}
+                       resizeMode='contain'
+                       source={require('../images/heart.png')} />
+              </TouchableOpacity>
+            </View>
+            <View>
+              <Text style={style.handle}>Profreshional Model, Treats!</Text>
+              <Text style={style.handle}>@beners</Text>
+            </View>
+            <View>
+              <Text style={style.bio}>
+  {`She a bad bad n she allllready know
+  (ya she know it)
+  Whole bank accooount I'll blow it 💸`}
+              </Text>
+            </View>
+          </ScrollView>
+        </LinearGradient>
+      </Animated.View>
     )
   }
 }
