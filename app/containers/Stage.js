c7d6615bde2fb5e2f2bc8624f87ee158e19b23d4
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-11-05 13:35:18 +0900

    Revert Stage.js to a simple stage

--- app/containers/Stage.js
+++ app/containers/Stage.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import React, {Component} from 'react'
+import React, {PureComponent} from 'react'
 import {connect}          from 'react-redux'
 import Login              from './Login'
 import Recs               from './Recs'
@@ -16,30 +16,27 @@ import {
 
 const useScratch = false
 
-class Stage extends Component {
+class Stage extends PureComponent {
   constructor(props) {
     super(props)
-    this.state = {
-      fadeIn: new Animated.Value(0),
-    }
     this.showScene = this.showScene.bind(this)
   }
 
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.nextScene && nextProps.nextScene.animation) {
-      this.animateIn(nextProps.nextScene.animation)
-    }
+  render() {
+    const {props} = this
+
+    return !props.hydrated ?
+      null
+    : useScratch ?
+      <Scratch />
+    :
+    <View style={[style.container]}>
+      { this.showScene(props.scene) }
+    </View>
   }
 
-  shouldComponentUpdate(props) {
-    return props.authenticated != this.props.authenticated
-  }
-
-  showScene(scene, props) {
-    props = {
-      ...props,
-      ...this.props,
-    }
+  showScene(scene) {
+    const {props} = this
 
     if (!props.authenticated) {
       return <Login />
@@ -71,95 +68,24 @@ class Stage extends Component {
       </Navigation>
     )
   }
-
-  animateIn(anim) {
-    const ogv = this.state[anim]._value
-    var params
-
-    switch (anim) {
-      case 'fadeIn':
-        params = {toValue: 1, duration: 333}
-        break
-      default:
-        params = {toValue: this.state[anim], duration: 0}
-    }
-
-    Animated.timing(
-      this.state[anim],
-      params
-    ).start(() => {
-      this.state[anim].setValue(ogv)
-      this.props.onTransition({
-        ...this.props.nextScene,
-        next: null,
-        animation: null,
-      })
-    })
-  }
-
-  render() {
-    const {props, state} = this
-
-    return !props.hydrated ?
-      null
-    : useScratch ?
-      <Scratch />
-    :
-    <View style={[style.container]}>
-      <View style={[style.container]}>
-        { this.showScene(props.scene) }
-      </View>
-      {
-        props.nextScene
-        ?
-        <View style={style.overlay}>
-          {
-            props.nextScene.animation === 'fadeIn' ?
-              <Animated.View style={[style.container, {
-                opacity: state.fadeIn,
-                backgroundColor: 'white'
-              }]}>
-                { this.showScene(props.nextScene, {...props}) }
-              </Animated.View>
-            : this.showScene(props.nextScene, {...props})
-          }
-        </View>
-        :
-        null
-      }
-    </View>
-  }
 }
 
 function mapStateToProps(state) {
   return {
     authenticated: !!state.user.accessToken,
-    hydrated:      state.hydrated,
-    scene:         state.scene.name,
-    params:        state.scene,
-    user:          state.user,
-    nextScene:     state.scene.next,
+    hydrated:      !!state.hydrated,
   }
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    onTransition: (scene) => {
-      dispatch({type: 'scene:change', scene: scene})
-    }
+
   }
 }
 
 const style = StyleSheet.create({
   container: {
     flex: 1
-  },
-  overlay: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
   },
 })
 
