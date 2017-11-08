96c166f4d43219981522cb8cb6f01cc55cd19084
benBen <benmbenjamin@gmail.com>
2017-11-08 10:16:25 -0800

    wip: modify LoginView for new styling

--- app/components/LoginView.js
+++ app/components/LoginView.js
@@ -11,12 +11,16 @@ import {
 export default function(props) {
   return (
     <View style={style.container}>
-      <TouchableOpacity onPress={props.login}>
-        <Text style={style.button}>Login with Instagram</Text>
+      <View style={[style.logo]}>
+        <Text>LOGO</Text>
+      </View>
+
+      <TouchableOpacity onPress={props.login} style={[style.button, {marginBottom: 20}]}>
+        <Text style={[style.buttonText]}>LOGIN WITH INSTAGRAM</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity onPress={props.linkedinLogin}>
-        <Text style={style.button}>Login with LinkedIn</Text>
+      <TouchableOpacity onPress={props.linkedinLogin} style={style.button}>
+        <Text style={[style.buttonText]}>LOGIN WITH LINKEDIN</Text>
       </TouchableOpacity>
     </View>
   )
@@ -25,11 +29,34 @@ export default function(props) {
 const style = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'space-around',
+    justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: 'rgba(35,31,32,1)',
+  },
+
+  logo: {
+    height: 100,
+    width: 100,
+    marginBottom: 40,
+    backgroundColor: 'white',
   },
 
   button: {
-    color: 'blue',
+    color: 'rgba(35,31,32,1)',
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: 'white',
+    backgroundColor: 'white',
+    width: 220,
   },
+
+  buttonText: {
+    width: '100%',
+    textAlign: 'center',
+    color: 'rgba(35,31,32,1)',
+  }
 })
