6fbc028bb06a708e0685fe42a4809f02bb11fd74
benBen <benmbenjamin@gmail.com>
2017-11-29 10:28:39 -0800

    checkpoint: main styling structures for editor

--- app/containers/Settings.js
+++ app/containers/Settings.js
@@ -111,6 +111,7 @@ class Settings extends Component {
     return (
       <SettingsView {...this.state}
                     {...this.props}
+                    scene={{view: 'Editor'}}
                     setBio={text => this.setState({bio: text})}
                     setDob={date => this.setState({dob: date})}
                     updateBio={this.updateBio}
--- app/containers/SettingsEditor.js
+++ app/containers/SettingsEditor.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react'
+import { mayteBlack } from '../constants/colors'
+import {
+  em,
+  statusBarHeight,
+  screenWidth,
+} from '../constants/dimensions'
 import {
   TouchableOpacity,
   StyleSheet,
+  ScrollView,
+  Image,
   View,
   Text,
 } from 'react-native'
@@ -10,15 +18,43 @@ export default class SettingsEditor extends Component {
   render() {
     const { props, state } = this
     return(
-      <View style={style.centered}>
-        <TouchableOpacity onPress={props.viewProfile}>
-          <Text>View Profile</Text>
-        </TouchableOpacity>
-        <Text>Editor</Text>
-        <TouchableOpacity onPress={props.viewPreferences}>
-          <Text>View Preferences</Text>
-        </TouchableOpacity>
-      </View>
+      <ScrollView style={[style.container, {backgroundColor: mayteBlack(), paddingLeft: em(1), paddingRight: em(1),}]}>
+        <View style={[style.header]}>
+          <TouchableOpacity>
+            <Text style={[style.headerBtn]}>CANCEL</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={[style.headerBtn]}>SAVE</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View>
+          <View style={[style.sectionHeader, {flexDirection: 'row'}]}>
+            <Text style={[style.sectionHeaderLabel]}>PHOTOS</Text>
+            <Text style={[style.sectionHeaderSublabel]}>{props.user.photos.length}/10</Text>
+          </View>
+          <View style={[style.currentPhotos]}>
+          {
+            props.user.photos.map((p,i) => {
+              return(
+                <Image style={style.currentPhotosImg} key={i} source={{uri: p.url}} />
+              )
+            })
+          }
+          </View>
+        </View>
+
+
+        <View style={style.centered}>
+          <TouchableOpacity onPress={props.viewProfile}>
+            <Text style={[style.text]}>View Profile</Text>
+          </TouchableOpacity>
+          <Text style={[style.text]}>Editor</Text>
+          <TouchableOpacity onPress={props.viewPreferences}>
+            <Text style={[style.text]}>View Preferences</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     )
   }
 }
@@ -28,9 +64,47 @@ const style = StyleSheet.create({
     flex: 1,
     // backgroundColor: mayteBlack(),
   },
+  text: {
+    color: 'white',
+  },
   centered: {
     flex:           1,
     alignItems:     'center',
     justifyContent: 'center',
   },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingTop: statusBarHeight + em(1),
+    marginBottom: em(2),
+  },
+  headerBtn: {
+    color: 'white',
+    fontSize: em(1),
+  },
+  sectionHeader: {
+    paddingBottom: em(0.66),
+    alignItems: 'center',
+  },
+  sectionHeaderLabel: {
+    color: 'white',
+    fontWeight: '700',
+    fontSize: em(1.33),
+  },
+  sectionHeaderSublabel: {
+    color: 'white',
+    marginLeft: em(0.66),
+  },
+
+
+  currentPhotos: {
+    flexDirection: 'row'
+  },
+  currentPhotosImg: {
+    width: screenWidth * 0.1,
+    height: screenWidth * 0.1,
+    borderRadius: em(0.33),
+    marginRight: em(0.33),
+  }
 })
