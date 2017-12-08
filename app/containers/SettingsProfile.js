5b635a340cbb68adfa90848ce69243b81b6853b1
benBen <benmbenjamin@gmail.com>
2017-12-07 16:57:20 -0800

    add SettingsProfile header icons

--- app/containers/SettingsProfile.js
+++ app/containers/SettingsProfile.js
@@ -1,24 +1,35 @@
 import React, { Component } from 'react'
 import ProfileView from '../components/ProfileView'
-import { matchHeaderHeight, statusBarHeight } from '../constants/dimensions'
+import { em, matchHeaderHeight, statusBarHeight } from '../constants/dimensions'
 import {
   TouchableOpacity,
   StyleSheet,
+  Image,
   View,
   Text,
 } from 'react-native'
 
 export default class SettingsProfile extends Component {
+  componentDidMount() {
+    this.props.updateBaseScene('Profile')
+  }
+
   render() {
     const { props, state } = this
     return(
       <View style={style.container}>
-        <View style={[{height: matchHeaderHeight, paddingTop: statusBarHeight, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}]}>
-          <TouchableOpacity onPress={() => props.viewSettingsPage('Preferences')}>
-            <Text>Preferences</Text>
+        <View style={[style.header]}>
+          <TouchableOpacity onPress={() => props.viewSettingsPage('Preferences')}
+                            style={[style.headerBtn, {marginLeft: em(1)}]}>
+            <Image source={require('../images/gear.png')}
+                   resizeMode='contain'
+                   style={[style.headerIcon]} />
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => props.viewSettingsPage('Editor')}>
-            <Text>Editor</Text>
+          <TouchableOpacity onPress={() => props.viewSettingsPage('Editor')}
+                            style={[style.headerBtn, {marginRight: em(1)}]}>
+            <Image source={require('../images/edit.png')}
+                   resizeMode='contain'
+                   style={[style.headerIcon]} />
           </TouchableOpacity>
         </View>
         <ProfileView {...props} {...state}
@@ -33,6 +44,30 @@ const style = StyleSheet.create({
   container: {
     flex: 1,
     // backgroundColor: mayteBlack(),
+  },
+  header: {
+    marginTop: em(1.33),
+    paddingTop: statusBarHeight,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    position: 'absolute',
+    width: '100%',
+    height: 40,
+    top: 0,
+    zIndex: 1,
+  },
+  headerBtn: {
+    backgroundColor: 'rgba(255,255,255,0.9)',
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  headerIcon: {
+    width: '50%',
+    height: '50%',
   },
   centered: {
     flex:           1,
