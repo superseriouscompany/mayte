3f317b921817a3653d88c9ef19060b0a4f65ca7e
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-12-23 01:41:40 -1000

    Rough styling for paywall

--- app/components/PaywallView.js
+++ app/components/PaywallView.js
@@ -1,34 +1,57 @@
 'use strict'
 
 import React, {Component} from 'react'
+import Text from './Text'
+import {em} from '../constants/dimensions'
+import base from '../constants/styles'
 import {
   StyleSheet,
-  Text,
   TouchableOpacity,
   View,
 } from 'react-native'
 
 export default function(props) {
+  const product = (props.products || [])[0]
+
   return (
     <View style={styles.container}>
-
-      { (props.products || []).map((p, key) => (
-        <TouchableOpacity key={key} onPress={() => props.buy(p.identifier)}>
-          <Text>
-            {p.title}: {p.description} {p.priceString}
+      { product ?
+        <View style={styles.optionsCnr}>
+          <Text style={styles.teaser}>
+            There are only 100 spots available for our first members only party in LA. Payment starts Valentine's Day.
           </Text>
-        </TouchableOpacity>
-      ))}
 
-      <TouchableOpacity onPress={props.visitVipWall}>
-        <Text>I have a VIP Code</Text>
-      </TouchableOpacity>
+          <TouchableOpacity style={[base.button]} onPress={() => props.buy(product.identifier)}>
+            <Text style={[base.buttonText]}>Join for {product.priceString}/month</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.backdoor} onPress={props.visitVipWall}>
+            <Text>I have a VIP Code</Text>
+          </TouchableOpacity>
+        </View>
+      :
+        <View style={styles.loadingCnr}>
+          <ActivityIndicator size="large" />
+        </View>
+      }
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex:           1,
+  },
+  optionsCnr: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  teaser: {
+    padding: em(2),
+  },
+  backdoor: {
+    marginTop: em(1)
+  },
+  loadingCnr: {
     flex:           1,
     justifyContent: 'center',
     alignItems:     'center',
---
+++ app/constants/styles.js
@@ -0,0 +1,14 @@
+export default {
+  button: {
+    backgroundColor: 'black',
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingTop: 5,
+    paddingBottom: 5,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
+  },
+}
