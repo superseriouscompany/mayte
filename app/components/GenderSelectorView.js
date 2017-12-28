e34e69dd11b1e5b08db4ee388c1c3db29f563084
benBen <benmbenjamin@gmail.com>
2017-12-28 11:28:07 -0800

    add stars and modify orbit anim

--- app/components/GenderSelectorView.js
+++ app/components/GenderSelectorView.js
@@ -4,7 +4,7 @@ import React, {Component} from 'react'
 import Text from './Text'
 import base from '../constants/styles'
 import {em, screenWidth, screenHeight} from '../constants/dimensions'
-import {mayteBlack} from '../constants/colors'
+import {mayteBlack, mayteWhite} from '../constants/colors'
 import {
   Image,
   ActivityIndicator,
@@ -52,7 +52,7 @@ function Reference(props) {
   )
 }
 
-function Option(props) {
+function Unicorn(props) {
   const selected = props[props.field] == props.value
 
   return (
@@ -60,6 +60,72 @@ function Option(props) {
       {props.children}
     </TouchableOpacity>
   )
+}
+
+class NightSky extends Component {
+  constructor(props) {
+    super(props)
+    const numStars = 5
+    this.stars = []
+    for (let i = 0; i < numStars; i++) {
+      this.stars.push({
+        top: Math.random() * screenHeight * 0.8,
+        left: Math.random() * screenWidth * 0.8 + screenHeight * 0.1,
+        twinkleDelay: Math.random() * 2000,
+      })
+    }
+  }
+  render() {
+    const {props,state} = this
+    return(
+      <View style={[style.nightSky]}>
+        <Star style={{top: em(2), left: em(2)}} twinkleDelay={2000} />
+        <Star style={{top: em(10), left: em(10)}} twinkleDelay={2800} />
+        <Star style={{top: em(20), left: em(15)}} twinkleDelay={3300} />
+        <Star style={{top: em(15), left: em(20)}} twinkleDelay={4500} />
+        <Star style={{top: em(23), left: em(2)}} twinkleDelay={5300} />
+        <Star style={{top: em(2), left: em(2)}} twinkleDelay={6200} />
+      </View>
+    )
+  }
+}
+
+class Star extends Component {
+  constructor(props) {
+    super(props)
+    this._twinkle = new Animated.Value(0)
+  }
+  componentDidMount() {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(this._twinkle, {
+          toValue: 1,
+          duration: 200,
+          delay: this.props.twinkleDelay || 1000,
+          easing: Easing.linear,
+        }),
+        Animated.timing(this._twinkle, {
+          toValue: 0,
+          duration: 300,
+          easing: Easing.linear,
+        })
+      ])
+    ).start()
+  }
+  render() {
+    const {props,state} = this
+    return(
+      <View style={[style.star, props.style]}>
+        <Animated.View style={[
+          style.twinkle,
+          {transform: [
+            {scale: this._twinkle},
+            {rotate: '45deg'}
+          ]}
+        ]} />
+      </View>
+    )
+  }
 }
 
 export default function(props) {
@@ -72,6 +138,7 @@ export default function(props) {
       :
         <View style={style.container}>
           <View style={style.identityCont}>
+            <NightSky />
             <Text style={style.heading}>I identify as:</Text>
             <SelfOption {...props} field="gender" value="female"
                         style={{top: em(6), right: em(2.5)}}>
@@ -90,24 +157,24 @@ export default function(props) {
           <View style={style.cornCont}>
             <View style={style.ground}></View>
             <Text style={style.heading}>{`I'm interested in:`}</Text>
-            <Option {...props} field="gender" value="male"
+            <Unicorn {...props} field="gender" value="male"
                     style={[style.corn, {left: em(1), bottom: em(4.5)}]}>
               <Image source={require('../images/unicorn-male-white.png')}
                      style={{width: '100%', height: '100%'}}
                      resizeMode='contain' />
-            </Option>
-            <Option {...props} field="gender" value="null"
+            </Unicorn>
+            <Unicorn {...props} field="gender" value="null"
                     style={[style.corn, {right: em(8.5), bottom: em(9)}]}>
             <Image source={require('../images/unicorn-all-white.png')}
                    style={{width: em(8), height: em(8), transform: [{scaleX:-0.66}, {scaleY: 0.66}]}}
                    resizeMode='contain' />
-            </Option>
-            <Option {...props} field="gender" value="female"
+            </Unicorn>
+            <Unicorn {...props} field="gender" value="female"
                     style={[style.corn, {right: em(1), transform: [{scaleX:-1.1},{scaleY:1.1}], bottom: em(3)}]}>
               <Image source={require('../images/unicorn-female-white.png')}
                       style={{width: '100%', height: '100%'}}
                       resizeMode='contain' />
-            </Option>
+            </Unicorn>
           </View>
 
           <TouchableOpacity style={[base.button, style.continue]} onPress={props.select}>
@@ -123,16 +190,43 @@ class SelfOption extends Component {
   constructor(props) {
     super(props)
     this._rotation = new Animated.Value(0)
+    this.loop = null
+    this.startLoop = this.startLoop.bind(this)
+    this.stopLoop = this.stopLoop.bind(this)
   }
 
   componentDidMount() {
-    Animated.loop(
+    if (this.props.gender == this.props.value) {
+      this.startLoop()
+    }
+  }
+
+  componentDidUpdate() {
+    if (this.props.gender == this.props.value) {
+      if (!this.loop) {
+        this.startLoop()
+      }
+    } else {
+      if (this.loop) {
+        this.stopLoop()
+      }
+    }
+  }
+
+  startLoop() {
+    this.loop = Animated.loop(
       Animated.timing(this._rotation, {
         toValue: 100,
-        duration: 4000,
-        easing: Easing.linear,
+        duration: 1000,
       })
-    ).start()
+    )
+    this.loop.start()
+  }
+
+  stopLoop() {
+    this.loop.stop()
+    this.loop = null
+    this._rotation = new Animated.Value(0)
   }
 
   render() {
@@ -140,7 +234,7 @@ class SelfOption extends Component {
     const selected = props[props.field] == props.value
 
     var interpolatedRotateAnimation = this._rotation.interpolate({
-        inputRange: [0, 100],
+      inputRange: [0, 100],
       outputRange: ['0deg', '360deg']
     });
 
@@ -161,6 +255,7 @@ class SelfOption extends Component {
 }
 
 const idDiameter = em(5)
+const starDiameter = em(0.33)
 
 const style = StyleSheet.create({
   container: {
@@ -185,13 +280,10 @@ const style = StyleSheet.create({
     marginTop: em(3),
     textAlign: 'center',
     color: 'white',
+    backgroundColor: 'transparent',
   },
-  options: {
-    marginBottom: em(2),
-  },
-  option: {
-    padding: em(1),
-  },
+
+
   identityCont: {
     height: screenHeight * 0.5,
     width: screenWidth,
@@ -227,9 +319,32 @@ const style = StyleSheet.create({
     backgroundColor: 'white',
     top: idDiameter/8,
   },
+  nightSky: {
+    position: 'absolute',
+    top: 0, bottom: 0,
+    left: 0, right: 0,
+  },
+  star: {
+    position: 'absolute',
+    backgroundColor: mayteWhite(),
+    width: starDiameter,
+    height: starDiameter,
+    borderRadius: starDiameter/2,
+  },
+  twinkle: {
+    backgroundColor: mayteWhite(0.8),
+    width: starDiameter*2,
+    height: starDiameter*2,
+    top: -(starDiameter/2),
+    left: -(starDiameter/2),
+  },
+
+
+
   cornCont: {
     height: screenHeight * 0.5,
     width: screenWidth,
+    backgroundColor: 'transparent',
   },
   corn: {
     width: em(8),
@@ -246,5 +361,5 @@ const style = StyleSheet.create({
     height: 185,
     bottom: 0,
     left: 0,
-  }
+  },
 })
