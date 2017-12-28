5f1500ce339da30e9d4a365af511233636eb27fc
benBen <benmbenjamin@gmail.com>
2017-12-28 14:10:33 -0800

    faces makes things better

--- app/components/PaywallView.js
+++ app/components/PaywallView.js
@@ -15,24 +15,26 @@ export default function(props) {
   const product = (props.products || [])[0]
 
   return (
-    <View style={styles.container}>
+    <View style={style.container}>
       { product ?
-        <View style={styles.container}>
-          <Text style={styles.teaser}>
-            There are only 100 spots available for our first members only party in LA. Payment starts Valentine's Day.
+        <View style={[style.container, {alignItems: 'center'}]}>
+          <Text style={style.teaser}>
+            There are only 100 spots available for our first members only party in LA.
           </Text>
 
-          <View style={styles.buttonsCnr}>
-            <TouchableOpacity style={[base.button, styles.mainButton]} onPress={() => props.buy(product.identifier)}>
-              <Text style={[base.buttonText]}>Join for {product.priceString}/month</Text>
+          <View style={style.payBtnCont}>
+            <TouchableOpacity style={[base.button, style.mainButton]} onPress={() => props.buy(product.identifier)}>
+              <Text style={[base.buttonText, style.payBtn]}>Join for {product.priceString}/month</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.backdoor} onPress={props.visitVipWall}>
-              <Text>I have a VIP Code</Text>
-            </TouchableOpacity>
+            <Text>{`Payment starts Valentine's Day.`}</Text>
           </View>
+
+          <TouchableOpacity style={style.backdoor} onPress={props.visitVipWall}>
+            <Text>I have a VIP Code</Text>
+          </TouchableOpacity>
         </View>
       :
-        <View style={styles.loadingCnr}>
+        <View style={style.loadingCnr}>
           <ActivityIndicator size="large" />
         </View>
       }
@@ -40,21 +42,26 @@ export default function(props) {
   )
 }
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
   container: {
     flex: 1,
   },
   teaser: {
-    padding: em(2),
-    flex: 1,
+    padding: em(1.66),
+    fontSize: em(2),
   },
   loadingCnr: {
     flex:           1,
     justifyContent: 'center',
     alignItems:     'center',
   },
-  buttonsCnr: {
+  payBtnCont: {
+    flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
+  },
+  payBtnText: {
+    textAlign: 'center'
   },
   mainButton: {
     marginBottom: em(1),
--- app/constants/styles.js
+++ app/constants/styles.js
@@ -1,14 +1,15 @@
+import {em} from './dimensions'
 export default {
   button: {
     backgroundColor: 'black',
-    paddingLeft: 10,
-    paddingRight: 10,
-    paddingTop: 5,
-    paddingBottom: 5,
-    borderRadius: 5,
+    paddingLeft: em(0.8),
+    paddingRight: em(0.8),
+    paddingTop: em(0.6),
+    paddingBottom: em(0.6),
+    borderRadius: em(0.33),
   },
   buttonText: {
     color: 'white',
-    fontSize: 20,
+    fontSize: em(1.25),
   },
 }
