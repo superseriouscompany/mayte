9246d884e0f91ba06a3b2e2ca5a1acdc9b0f8442
benBen <benmbenjamin@gmail.com>
2018-01-22 22:32:24 -0800

    replace unicorn icon wow much better

--- app/components/DeadView.js
+++ app/components/DeadView.js
@@ -2,27 +2,64 @@
 
 import React, {Component} from 'react'
 import {ButtonBlack}      from './Button'
+import {ParticleSheet}    from './Particle'
+import {
+  em,
+  screenWidth,
+  bottomBoost,
+} from '../constants/dimensions'
 import {
   Linking,
   StyleSheet,
   Text,
+  Image,
   View,
 } from 'react-native'
 
 export default function(props) {
   return (
-    <View style={styles.container}>
-      <ButtonBlack text="Ya Burnt"
+    <View style={style.container}>
+      <ParticleSheet count={6} loopLegnth={10000} scale={0.4} particleStyle={{opacity: 0.4}} />
+      <ParticleSheet count={6} loopLength={15000} scale={0.8} particleStyle={{opacity: 0.8}} />
+      <ParticleSheet count={6} loopLength={20000} scale={1} />
+
+      <Text style={[style.text, style.header]}>{`Please Upgrade`}</Text>
+      <Text style={[style.text, style.body]}>{`At Unicorn, we strive to deliver a consistent, quality experience for all of our users; unfortunately, this sometimes requires us to deprecate older versions of the app. We apologize for any inconvenience.`}</Text>
+      <ButtonBlack text="Upgrade Now"
         onPress={() => Linking.openURL('https://dateunicorn.com/download')}/>
+
+      <Image style={[style.icon]}
+             source={require('../images/unicorn-icon-black.png')}
+             resizeMode="contain" />
     </View>
   )
 }
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
   container: {
     flex          : 1,
     justifyContent: 'center',
     alignItems    : 'center',
   },
-
+  text: {
+    backgroundColor: 'transparent',
+    fontFamily: 'Futura',
+    textAlign: 'center',
+  },
+  header: {
+    fontSize: em(2),
+    marginBottom: em(2),
+  },
+  body: {
+    fontSize: em(1),
+    marginBottom: em(2),
+    width: '80%',
+  },
+  icon: {
+    height: em(3),
+    width: em(3),
+    position: 'absolute',
+    bottom: em(2) + bottomBoost,
+    left: screenWidth / 2 - em(1.5),
+  }
 })
