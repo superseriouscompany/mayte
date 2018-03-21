9951f6d160f34ea6b5a744d19508efbd6e59cdb5
benBen <benmbenjamin@gmail.com>
2018-03-21 12:41:58 -0700

    acceptable base

--- app/components/RecsCheerleaderView.js
+++ app/components/RecsCheerleaderView.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import LinearGradient from 'react-native-linear-gradient'
 import {
   screenHeight,
   screenWidth,
@@ -6,9 +7,14 @@ import {
   notchHeight,
 } from '../constants/dimensions'
 import {
+  mayteWhite,
+  mayteBlack,
+} from '../constants/colors'
+import {
   StyleSheet,
   Text,
   View,
+  Image,
   ScrollView,
 } from 'react-native'
 
@@ -22,15 +28,19 @@ export default class RecsCheerleaderView extends Component {
           style={styles.scroll}
           contentContainerStyle={styles.scrollCnr}
           showsVerticalScrollIndicator={false}>
-        { (props.recs||[]).map((r,i) =>
-          <View key={i} style={[styles.rec, {
-            backgroundColor: i%2 ? 'indianred' : 'navajowhite',
-            marginLeft: i%2 ? em(0.25) : em(0.5),
-            marginRight: i%2 ? em(0.5) : em(0.25),
-            transform: [{translateY: i%2 ? em(3) : 0}]
-          }]}>
-            <Text>{r.fullName}</Text>
-          </View> ) }
+        { (props.recs.filter(u => u.photos[0].url)||[]).map((r,i) => {
+          return (
+            <View key={i} style={[styles.rec, {
+              marginLeft: i%2 ? em(0.25) : em(0.5),
+              marginRight: i%2 ? em(0.5) : em(0.25),
+              transform: [{translateY: i%2 ? tileOffset : 0}]
+            }]}>
+              <Image style={styles.recImg} source={{url: r.photos[0].url}} resizeMode={'cover'} />
+              <LinearGradient colors={[mayteBlack(0), mayteBlack(0.5)]} style={[styles.recGrad]} />
+              <Text style={styles.recName}>{r.fullName.split(' ')[0]}</Text>
+            </View>
+          )
+        }) }
         </ScrollView>
       </View>
     )
@@ -42,24 +52,14 @@ const recDims = {
   height: screenHeight / 2.5 - em(0.75),
 }
 
+const tileOffset = em(3)
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  scroll: {
-    width: '100%',
-    flex: 1,
-    backgroundColor: 'lightblue'
-  },
-  scrollCnr: {
-    paddingTop: em(0.5) + notchHeight,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-  },
-  rec: {
-    ...recDims,
-    marginBottom: em(0.5),
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
+  container: { flex: 1 },
+  scroll: { width: '100%', flex: 1, },
+  scrollCnr: { paddingTop: em(0.5) + notchHeight, paddingBottom: tileOffset, flexDirection: 'row', flexWrap: 'wrap', },
+  rec: { ...recDims, marginBottom: em(0.5), justifyContent: 'flex-end', paddingBottom: em(1), alignItems: 'center', },
+  recImg: { position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 },
+  recGrad: { position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 },
+  recName: { fontFamily: 'futura', fontSize: em(1), color: mayteWhite(), backgroundColor: 'transparent', },
 })
